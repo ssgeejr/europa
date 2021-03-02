40b23b9b474eb77438b414b3ffee3e83683b4942
Steven Gee <ioexcept@gmail.com>
2021-03-02 20:22:31 +0000

    working features all around

---
+++ client/src/main/java/com/eightonefournorth/europa/client/Test.java
@@ -0,0 +1,7 @@
+package com.eightonefournorth.europa.client;
+
+public class Test{
+  public String getDate(){
+	  return new java.util.Date().toString();
+  }
+}
--- mysql/init.sql
+++ mysql/init.sql
@@ -1,9 +1,11 @@
-CREATE TABLE student (
-	    id int,
-	    name VARCHAR(255)
+CREATE TABLE clients (
+	    clientid int AUTO_INCREMENT primary key NOT NULL,
+	    name VARCHAR(64) NOT NULL,
+	    email VARCHAR(255) NOT NULL,
+	    tracking_key VARCHAR(255) NOT NULL 
 );
 
-INSERT INTO student(id, name) VALUES
-(1,'A'),
-(2,'B'),
-(3,'C');
+INSERT INTO clients(name,email,tracking_key) VALUES
+('Some User', 'someuser@mail.com','4d0c733ca9f0f43fcebe3d33cc4ae4e4'),
+('Another User','anotheruser@dodoo.com','7702b3548f8f64b2cd395745d8f3eaa4'),
+('Me You', 'me@you.com','ca013b30f4ef743d22e6e91853c441cf');
