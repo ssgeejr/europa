bf4c3f5d9840719ffed183ce7a2178c35f473d16
Steven Gee <ioexcept@gmail.com>
2021-03-02 02:04:21 +0000

    pushing for safety

---
+++ europaserver/src/main/java/com/eightonefournorth/europa/server/EuropaServer.java
@@ -0,0 +1,16 @@
+package com.eightonefournorth.europa.server;
+
+public class EuropaServer{
+
+public EuropaServer(){
+	try{
+		System.out.println("LOOK AT ME GO!!!!");
+	}catch(Exception ex){
+		ex.printStackTrace();
+	}
+}
+
+public static void main(String args[]){
+	new EuropaServer();
+}
+}
