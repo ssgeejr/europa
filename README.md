0146ae6e511c0c0ad92bf54b1c7e7daa9c8ea474
Steven Gee <ioexcept@gmail.com>
2021-03-04 15:45:52 +0000

    added scheme to init.sql

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # europa-
 Java Phishing Client/Server application based on a docker foundation 
+
+
+### Code example for browser version checking
+
+```
+<div id="example"></div>
+
+<script type="text/javascript">
+
+txt = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+txt+= "<p>Browser Name: " + navigator.appName + "</p>";
+txt+= "<p>Browser Version: " + navigator.appVersion + "</p>";
+txt+= "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+txt+= "<p>Platform: " + navigator.platform + "</p>";
+txt+= "<p>User-agent header: " + navigator.userAgent + "</p>";
+
+document.getElementById("example").innerHTML=txt;
+
+</script>
+```
+
+
+### References
+
+https://www.seanmcp.com/articles/how-to-get-the-browser-version-in-javascript/
--- mysql/init.sql
+++ mysql/init.sql
@@ -1,11 +1,22 @@
-CREATE TABLE clients (
-	    clientid int AUTO_INCREMENT primary key NOT NULL,
-	    name VARCHAR(64) NOT NULL,
-	    email VARCHAR(255) NOT NULL,
-	    tracking_key VARCHAR(255) NOT NULL 
+CREATE TABLE contacts (
+            contactid int AUTO_INCREMENT primary key NOT NULL,
+            name VARCHAR(64) NOT NULL,
+            email VARCHAR(128) NOT NULL,
+            hash VARCHAR(32) NOT NULL
 );
 
-INSERT INTO clients(name,email,tracking_key) VALUES
+CREATE TABLE responses (
+	reseponseid int AUTO_INCREMENT primary key NOT NULL,
+	hash VARCHAR(32) NOT NULL,
+	event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	browser_codename VARCHAR(32) NOT NULL,
+	browser_name VARCHAR(32) NOT NULL,
+	platform VARCHAR(16) NOT NULL,
+	browser_version VARCHAR(128) NOT NULL,
+	agent_header VARCHAR(128) NOT NULL
+);
+
+INSERT INTO clients(name,email,hash) VALUES
 ('Some User', 'someuser@mail.com','4d0c733ca9f0f43fcebe3d33cc4ae4e4'),
 ('Another User','anotheruser@dodoo.com','7702b3548f8f64b2cd395745d8f3eaa4'),
 ('Me You', 'me@you.com','ca013b30f4ef743d22e6e91853c441cf');
