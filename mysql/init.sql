4435bf5a609eafe8f5fc4f8cb2bc825133ba8c86
Steven Gee <ioexcept@gmail.com>
2021-03-11 13:53:43 +0000

    updated build script to create company and fetch keys, then insert new primary keys

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,23 +1,36 @@
+set @CID = -1;
+
+CREATE TABLE company (
+            companyid int AUTO_INCREMENT primary key NOT NULL,
+            name VARCHAR(64) NOT NULL
+);
+
 CREATE TABLE contacts (
             contactid int AUTO_INCREMENT primary key NOT NULL,
+            companyid int NOT NULL,
             name VARCHAR(64) NOT NULL,
             email VARCHAR(128) NOT NULL,
             hash VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE responses (
-	reseponseid int AUTO_INCREMENT primary key NOT NULL,
-	hash VARCHAR(32) NOT NULL,
-	event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	browser_codename VARCHAR(32) NOT NULL,
-	browser_name VARCHAR(32) NOT NULL,
-	platform VARCHAR(16) NOT NULL,
-	browser_version VARCHAR(128) NOT NULL,
-	agent_header VARCHAR(128) NOT NULL
+        reseponseid int AUTO_INCREMENT primary key NOT NULL,
+        hash VARCHAR(32) NOT NULL,
+        event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        browser_codename VARCHAR(32) NOT NULL,
+        browser_name VARCHAR(32) NOT NULL,
+        platform VARCHAR(16) NOT NULL,
+        browser_version VARCHAR(128) NOT NULL,
+        agent_header VARCHAR(128) NOT NULL
 );
 
+INSERT INTO company(name) VALUES('SERPENT');
 
-INSERT INTO contacts(name,email,hash) VALUES
-('Some User', 'someuser@mail.com','4d0c733ca9f0f43fcebe3d33cc4ae4e4'),
-('Another User','anotheruser@dodoo.com','7702b3548f8f64b2cd395745d8f3eaa4'),
-('Me You', 'me@you.com','ca013b30f4ef743d22e6e91853c441cf');
+select max(companyid) into @CID from company;
+
+
+INSERT INTO contacts(companyid, name,email,hash) VALUES
+(@CID ,'Some User', 'someuser@mail.com', '4d0c733ca9f0f43fcebe3d33cc4ae4e4'),
+(@CID ,'Another User', 'anotheruser@dodoo.com', '7702b3548f8f64b2cd395745d8f3eaa4'),
+(@CID , 'Me You', 'me@you.com', 'ca013b30f4ef743d22e6e91853c441cf');
+
